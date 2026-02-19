5bb0e8d89b16f28aec1cce779c586af8d5321d17
Durojaiye Toluwanimi <121799434+davistolu@users.noreply.github.com>
2026-02-19 13:01:32 +0100

    Add Analytics model to Mongoose exports

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,9 +2,10 @@
 import User from './User';
 import Resource from './Resource';
 import Notification from './Notification';
+import Analytics from './Analytics';
 
 // Export all models for convenience
-export { User, Resource, Notification };
+export { User, Resource, Notification, Analytics };
 
-// Note: Make sure all models are imported here to ensure they are registered with Mongoose
+// Note: Make sure all models are registered with Mongoose
 // This file should be imported in your main server file or database configuration
